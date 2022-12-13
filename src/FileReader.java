c95e207cd196ed1d085f179473f4af2696d170e6
Oleg <Oleg-BLR@yandex.ru>
2022-12-13 10:01:45 +0300

    Solution02 with just many methods working

--- src/FileReader.java
+++ src/FileReader.java
@@ -9,41 +9,15 @@ public class FileReader {
 
     public void getDataFromFile(Path path) throws IOException {
 //      1. Read file:
-//        String contentsString = Files.readString(path);
-//        System.out.println("Read file: \n" + contentsString);
-
         String rawText = readFile(path);
 
 //     2. Format string data from file into one string:
-//        String formattedData = contentsString.
-//                replaceAll("\\n", ",")
-//                .replaceAll("\\s", "");
-//        System.out.println("One string data: " + formattedData);
-
         String oneString = oneString(rawText);
 
 //      3. Parse string for key-value pairs
-//        Map<String, String> stringToMap = Arrays
-//                .stream(formattedData.split(","))
-//                .map(entry -> entry.split(":"))
-//                .collect(Collectors.toMap(entry -> entry[0], entry -> entry[1]));
-//        System.out.println("string to map: " + stringToMap);
-
         Map<String, String> stringToMap = stringToMap(oneString);
-
-//      4. Create Profile
-//        Profile profile = new Profile();
-//
-//        profile.setAge(Integer.parseInt(stringToMap.get("Age")));
-//        profile.setName(stringToMap.get("Name"));
-//        profile.setEmail(stringToMap.get("Email"));
-//        profile.setPhone(Long.parseLong(stringToMap.get("Phone")));
-//
-//        System.out.println("Profile: " + profile);
-
         Profile profile = profile(stringToMap);
         System.out.println(profile);
-
     }
 
     private String readFile(Path path) {
